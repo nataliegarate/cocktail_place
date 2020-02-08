841089caa4ce4b6666e3b1439d22082f1bb056d5
nataliegarate <natalieegarate@hotmail.com>
2020-02-07 22:32:49 -0800

    added index file to store

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {createLogger} from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import cocktail from './cocktail'
+
+const reducer = combineReducers({cocktail})
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+)
+const store = createStore(reducer, middleware)
+
+export default store
+export * from './cocktail'
